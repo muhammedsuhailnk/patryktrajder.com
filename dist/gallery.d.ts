7528b3c4f1eb3f3fa8cd250f0575bbcd1211eef2
Jakub Janowski <jkjanowski@gmail.com>
2020-04-17 21:47:50 +0200

    added dist to .gitignore

--- dist/gallery.d.ts
+++
@@ -1,9 +0,0 @@
-declare const abortClickDistance: number;
-declare const bodyOverflow: string;
-declare const galleries: HTMLCollectionOf<Element>;
-declare function setUpSlider(slider: HTMLElement): void;
-declare function setUpSliderItem(item: HTMLImageElement, slider: HTMLElement): void;
-declare function showFullImage(img: HTMLImageElement): void;
-declare function showPreview(img: HTMLImageElement): void;
-declare function setUpZoom(full: HTMLElement): void;
-declare function zoomOut(full: HTMLElement, img: HTMLImageElement): void;
