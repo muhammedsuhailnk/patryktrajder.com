e7de2139bd26105a274df271bf204c42e5f526aa
Jakub Janowski <jkjanowski@gmail.com>
2020-06-02 17:21:52 +0200

    langueage select

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Constants from "./scripts/constants";
 import Expander from "./scripts/expander";
 import Gallery from "./scripts/gallery";
+import LanguageDropdown from "./scripts/language-dropdown";
 import Slider from "./scripts/slider";
 
 const banners = document.getElementsByClassName("banner");
@@ -23,6 +24,10 @@ for (let i = 0; i < galleries.length; i++) new Gallery(galleries[i]);
 const expanders = document.getElementsByClassName("expander");
 for (let i = 0; i < expanders.length; i++)
   new Expander(expanders[i] as HTMLElement);
+
+const languageDropdown = document.getElementsByClassName("language-dropdown");
+for (let i = 0; i < languageDropdown.length; i++)
+  new LanguageDropdown(languageDropdown[i] as HTMLElement);
 
 // import Promotion from "./scripts/promotion";
 // const products = document.getElementsByClassName("product-details");
