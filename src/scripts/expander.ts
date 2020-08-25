37363c7e62b893b7acc83679e76f287b2dbdfd0b
Jakub Janowski <jkjanowski@gmail.com>
2020-08-25 21:29:34 +0200

    added aria-label to expander button

--- src/scripts/expander.ts
+++ src/scripts/expander.ts
@@ -2,25 +2,29 @@ import Constants from "./constants";
 import Utils from "./utils";
 
 export default class Expander {
+  private readonly button: HTMLButtonElement;
   private readonly svgUse: SVGUseElement;
   private readonly container: HTMLElement;
   private isExpanded: boolean = false;
 
   constructor(private readonly expander: HTMLElement) {
-    let button = expander.querySelector("button") as HTMLElement;
+    this.button = expander.querySelector("button") as HTMLButtonElement;
     this.container = expander.querySelector(".container") as HTMLElement;
     this.svgUse = expander.querySelector("use") as SVGUseElement;
 
-    button.addEventListener("click", this.toggle);
+    this.button.setAttribute("aria-label", "Show more");
+    this.button.addEventListener("click", this.toggle);
     addEventListener("resize", this.handleWindowResize);
   }
 
   private toggle = () => {
     this.isExpanded = !this.isExpanded;
     if (this.isExpanded) {
+      this.button.setAttribute("aria-label", "Show less");
       this.container.style.height = this.container.scrollHeight + "px";
       this.svgUse.href.baseVal = "#icon-chevron-up";
     } else {
+      this.button.setAttribute("aria-label", "Show more");
       this.container.style.height = Constants.expanderMinHeight + "rem";
       this.svgUse.href.baseVal = "#icon-chevron-down";
       if (window.scrollY > this.expander.offsetTop)
